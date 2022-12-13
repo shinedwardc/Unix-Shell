be320590ac24ebec71bc4ddfc5924d2a563a4bb9
Edward Shin <shinedwardc@gmail.com>
2022-12-13 22:13:29 +0900

    Error checking

--- shell.c
+++ shell.c
@@ -23,6 +23,9 @@ char ** get_input(char * input){
     return command;
 }
 
+int cd(char * path){
+    return chdir(path);
+}
 
 int main(){
     char **command;
@@ -30,8 +33,15 @@ int main(){
     int stat_loc;
 
     while (1){
-        input = readline("shell> ");
+        input = readline("my_shell> ");
         command = get_input(input);
+
+        if (strcmp(command[0],"cd") == 0){
+            if (cd(command[1]) < 0){
+                perror(command[1]);
+            }
+            continue;
+        }
 
         pid_t child_pid = fork();
         if (child_pid < 0){
@@ -39,7 +49,10 @@ int main(){
             exit(1);
         }
         if (child_pid == 0){
-            execvp(command[0],command);
+            if (execvp(command[0],command) < 0){
+                perror(command[0]);
+                exit(1);
+            }
             printf("This won't be printed if execvp is successul\n");
         }
         else{
@@ -49,10 +62,6 @@ int main(){
         free(input);
         free(command);
     }
-    //char * argv[] = {"ls","-l","-h","-a",NULL};
-    //execvp(argv[0],argv);
-
-
 
     return 0;
 }
